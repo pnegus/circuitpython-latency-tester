cfff2abfb424428b56505822b28a0663a39fb288
pnegus <51723374+pnegus@users.noreply.github.com>
2023-11-25 16:46:36 -0500

    Update code.py
    modified testing methodology, also add delay before testing

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-# Write your code here :-)
 import board
 import digitalio
 from digitalio import DigitalInOut, Direction, Pull
@@ -20,21 +19,19 @@ mouse = Mouse(usb_hid.devices)
 while True:
     if not btn.value:
         led.value = False
-        start_calibration_timestamp = time.monotonic()
+        #give 3 seconds to let sensor sit on display
+        time.sleep(3)
         
-        #rudimentary calibration
+        #evaluate average luminance, ideally measures black level
+        #TODO: need to determine effective sampling rate. ALS-PT19 rise/fall time is .11 and .22 ms respectively (idk what that means)
+        start_calibration_timestamp = time.monotonic()
         initial_sensorvalue = 0.0
         n = 0
-        largest = 0
         while (time.monotonic() - start_calibration_timestamp <= 3.0):
             initial_sensorvalue += light_sensor.value
-            largest = max(largest, light_sensor.value)
             n += 1
         initial_sensorvalue = initial_sensorvalue / n
-        max_deviation = largest - initial_sensorvalue
-        print("Max calibration deviation: ")
-        print(max_deviation)
-        
+
         average_latency = 0.0
         num_runs = 100
         
@@ -43,7 +40,9 @@ while True:
             mouse.press(Mouse.LEFT_BUTTON)
             time.sleep(0.01)
             mouse.release(Mouse.LEFT_BUTTON)
-            while (time.monotonic() - start_test_timestamp <= 0.5 and light_sensor.value <= initial_sensorvalue + max_deviation * 1.25):
+            #gtg2% question mark?
+            #might need to clamp sampling frequency
+            while (time.monotonic() - start_test_timestamp <= 0.5 and light_sensor.value <= initial_sensorvalue + 0.02 * initial_sensorvalue):
                 continue;
             latency = (time.monotonic() - start_test_timestamp) * 1000
             average_latency += latency
